a52c546e6e112567558b17acff2031f1195098aa
Victoria Wagner <vs.wagner@yahoo.de>
2019-05-13 12:36:22 +0200

    first commit

---
+++ Assets/Scripts/RB_test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RB_test : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            rb.AddForce(new Vector3(0, 0, 10) * Time.deltaTime, ForceMode.VelocityChange);
+        } else if(Input.GetAxis("Vertical") < 0)
+        {
+            rb.AddForce(new Vector3(0, 0, -10) * Time.deltaTime, ForceMode.VelocityChange);
+        } else if(Input.GetAxis("Horizontal") > 0)
+        {
+            rb.AddForce(new Vector3(10, 0, 0) * Time.deltaTime, ForceMode.VelocityChange);
+        }else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.AddForce(new Vector3(-10, 0, 0) * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Vector3 pos;
+    public float speed = 10f;
+    private Vector3 moveVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float zDir = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        moveVector = new Vector3(xDir, 0, zDir);
+        transform.Translate(moveVector);
+    }
+}
